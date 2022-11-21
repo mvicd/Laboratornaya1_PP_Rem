771660dc729364938adaa5fad05a351093d973d7
mvicd <115185462+mvicd@users.noreply.github.com>
2022-11-21 10:48:16 +0400

    Подключение библиотек + создание соответствующих директорий

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from msilib.schema import Directory
+import string
+import cv2
+import requests
+import os
+from tqdm import tqdm
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin, urlparse
 
+directory_dataset = "D:\VS Code project\Images.py\dataset"
+directory_rose = "D:\VS Code project\Images.py\dataset\rose"
+directory_tulip = "D:\VS Code project\Images.py\dataset\tulip"
+
+
+def directory():
+    if not os.path.isdir("dataset"):
+        os.mkdir(directory_dataset)
+    if not os.path.isdir("rose"):
+        os.mkdir(directory_rose)
+    if not os.path.isdir("tulip"):
+        os.mkdir(directory_tulip)
