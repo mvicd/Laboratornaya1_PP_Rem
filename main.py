3243b152ed927d2d1b5afb091cccbc125a8ea990
mvicd <115185462+mvicd@users.noreply.github.com>
2022-11-21 11:03:09 +0400

    Выгрузка картинок и их сохранение

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import time
 from msilib.schema import Directory
 import string
 from random import random
@@ -7,6 +8,7 @@ import requests
 import os
 from tqdm import tqdm
 from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin, urlparse
 from urllib.parse import urljoin, urlparse
 
 directory_dataset = "D:\VS Code project\Images.py\dataset"
@@ -44,3 +46,41 @@ url_src = []
 
 for i in img:
     url_src.append(i.get("src"))
+
+
+def rose_load():
+    directory()
+    count = 1
+    s = 0
+    so = 1
+
+    for i in range(1, 51):
+        url = "https://yandex.ru/images/search?p=%d&text=rose&lr=51&rpt=image&uinfo=sw-1920-sh-1080-ww-1872-wh-932-pd-1-wp-16x9_1920x1080" % s
+
+        print(url)
+        s += 1
+        name = string.ascii_lowercase
+        str1 = ''.join(random.sample(name, 15))
+        rand_user = {'User-Agent': str1}
+
+        html_text = requests.get(url, headers=rand_user).text
+        soup = bs(html_text, 'html.parser')
+        img = soup.find_all('img', class_="serp-item__thumb justifier__thumb")
+
+        url_src = []
+
+        for j in img:
+            url_src.append(j.get("src"))
+
+        for k in tqdm(url_src):
+            url_absolut = "https:" + k
+            r = requests.get(url_absolut).content
+            fp = open(directory_rose + '/' + f"{so:05}" + '.jpg', "wb")
+            fp.write(r)
+            so += 1
+            time.sleep(0.25)
+            print(count, "/50")
+            count += 1
+
+
+rose_load()
